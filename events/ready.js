41d62a28dec771ed3e36c08c8d11773197d9414c
moderasyonbo <93944142+moderasyonbo@users.noreply.github.com>
2022-06-08 01:21:26 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,6 +9,6 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("dnd");
-  client.user.setActivity("youtube.com/GweepCreativeOfficial", { type: "WATCHING"}); //// TYPE - WATCHING , PLAYING , LISTENING gibi değiştirilebilir.
+  client.user.setActivity("adamsın novabotlist <3")
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
 };
